9d7d9fccbc7c68a2239cf13b6e85df29a0df845d
basselhilf <basselhilf@gmail.com>
2020-12-25 23:42:37 +0200

    React Input

---
+++ react/react-input-lesson-exercises/src/components/Exercise1.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Exercise1 extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            age: ""
+        }
+    }
+
+    showAlert = () => {
+        alert(`Come in ${this.state.name}, you're ${this.state.age} - that's good enough`)
+    }
+
+    handleInputChange = event => {
+        const {value, name} = event.target;    
+        this.setState({
+          [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <input id="name-input" name="name" onChange={this.handleInputChange}/>
+                <input id="age-input" name="age" onChange={this.handleInputChange}/>
+                <button onClick={this.showAlert}>Go to Bar</button>
+            </div>
+        );
+    }
+}
+
+export default Exercise1;
