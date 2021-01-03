973a0ca007b55c039ff5851b7c0813e220b53f23
basselhilf <basselhilf@gmail.com>
2021-01-03 12:36:16 +0200

    react

---
+++ react/react-hooks-lesson-exercises/src/exercises/Exercise2.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, axios } from 'react'
+
+export default function Exercise2() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    (async () => {
+      const users = await axios.get('https://randomuser.me/api/?results=5')
+      console.log(users.data.results);
+      setUsers(users.data.results)
+    })()
+
+
+  }, [])
+  return (
+    <div className="ex-space">
+      <h4 className="ex-title">Exercise 2</h4>
+      <div className="exercise" id="ex-2">
+        {users.map((u, i) => {
+          return (
+            <div key={i}>
+              <span>{u.name.first}</span>
+              <span>{u.name.last}</span>
+              <img src={u.picture.thumbnail} alt="" />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
